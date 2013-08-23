859f9ef4c1e6b78264af5a049ae3c729d9f1fa1d
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2013-08-23 15:34:32 -0700

    Use non-inlined function in benchmark

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,8 @@
 #include <immintrin.h>
 #include <cassert>
 
-#if USE_DOUBLE
-typedef double scalar_t;
-#else
-typedef float scalar_t;
-#endif
+#include "common.h"
+#include "gemm.h"
 
 using namespace std;
 
@@ -17,38 +14,18 @@ static const double frequency = 1.0e-9;
 static const int flops_per_calc = 2;
 static const int dim = 5;
 static const int nmat = 4992;
-static const int iters = 100000;
-static const int block_size = 32;
+static const int iters = 1000;
+static const int block_size = 16;
 
 scalar_t A[nmat*dim*dim] __attribute__((aligned(ALIGNMENT)));
 scalar_t B[nmat*dim*dim] __attribute__((aligned(ALIGNMENT)));
 scalar_t C[nmat*dim*dim] __attribute__((aligned(ALIGNMENT)));
 
-template <int M, int N, int K>
-void gemm(const scalar_t *A, const scalar_t *B, scalar_t *C, int nmat, int base, int block_size){
-  for(int i = 0; i < M; i++){
-    for(int j = 0; j < N; j++){
-      for(int k = 0; k < K; k++){
-	const scalar_t *sub_A = A + base + i*K*nmat + k*nmat;
-	const scalar_t *sub_B = B + base + k*N*nmat + j*nmat;
-	scalar_t *sub_C = C + base + i*N*nmat + j*nmat;
-
-#pragma unroll(4)
-#pragma vector aligned
-#pragma simd assert
-	for(int idx = 0; idx < block_size; idx++){
-	  C[idx] += A[idx]*B[idx];
-	}
-      }
-    }
-  }
-}
-
 void benchmark(){
   double gflop;
   double t0;
 
-  cout << "nmatrices: " << nmat << " block_size: " << block_size << " dimension: " << dim << " iterations:" << iters << endl;
+  cout << "nmatrices:" << nmat << " block_size:" << block_size << " dimension:" << dim << " iterations:" << iters << endl;
   gflop = frequency * flops_per_calc * iters * dim * dim * dim * nmat;
 
   assert(nmat % block_size == 0); // Ideal use case
@@ -60,7 +37,7 @@ void benchmark(){
   for(int i = 0; i < iters; i++){
 #pragma omp parallel for
     for(int z = 0; z < nmat; z+= block_size){
-      gemm<dim, dim, dim>(A, B, C, nmat, z, block_size);
+      gemm(A, B, C, dim, dim, dim, nmat, z, block_size);
     }
   }
 
